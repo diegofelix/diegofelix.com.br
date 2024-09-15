009aa4cc474c26dfe7f1c8397d09d60739aa5d6b
Diego Felix <diegoflx.oliveira@gmail.com>
2024-09-15 10:25:49 -0300

    feat: change my description

--- app/page.jsx
+++ app/page.jsx
@@ -11,17 +11,15 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span class="text-xl">Software Developer</span>
             <h1 className="h1">
               Hello I'm <br /> <span className="text-accent">Diego Felix</span>
             </h1>
             <p className="max-w-[500x] mb-9 text-white/80">
-              I excel at crafting elegant digital experiences and I am
-              proficient in various programming languages and technologies.
+              I have been working we web developer since 2009. I have a lot of
+              experience and I am currently a Tech Leader at Leroy Merlin
             </p>
-            {/* buttons and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
                 variant="outline"
