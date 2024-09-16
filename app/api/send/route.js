c3f324e8a1410ac76dade29b4d14b807c47ae31d
Diego Felix <diegoflx.oliveira@gmail.com>
2024-09-16 18:31:33 -0300

    feat: add recaptcha to avoid spams

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { EmailTemplate } from "../../../components/EmailTemplate";
 import { Resend } from "resend";
 
@@ -5,11 +6,28 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
   try {
-    const { firstName, lastName, email, message } = await req.json();
+    const { firstName, lastName, email, message, recaptchaToken } =
+      await req.json();
+
+    // Checks for recaptcha validation
+    const recaptchaResponse = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${recaptchaToken}`
+    );
+
+    console.log(recaptchaResponse);
+
+    const { success } = recaptchaResponse.data;
+
+    if (!success) {
+      return new Response(
+        JSON.stringify({ error: "reCAPTCHA validation failed" }),
+        { status: 400 }
+      );
+    }
 
     const { data, error } = await resend.emails.send({
       from: "Site Diego Felix <site@diegofelix.com.br>",
-      to: ["contato@diegofelx.com.br"],
+      to: ["contato@diegofelix.com.br"],
       subject: "Você recebeu um contato através do seu site",
       react: EmailTemplate({ firstName, lastName, email, message }),
     });
