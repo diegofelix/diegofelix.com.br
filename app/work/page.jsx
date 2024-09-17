889de3d8e2bc7a800abea4812d87283b7912f0e2
Diego Felix <diegoflx.oliveira@gmail.com>
2024-09-17 06:21:02 -0300

    fix: break line when text is bigger than view width

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -120,7 +120,7 @@ const Work = () => {
               </div>
 
               <p className="text-white/60">{project.description}</p>
-              <ul className="flex gap-4">
+              <ul className="flex gap-4 flex-wrap">
                 {project.stack.map((item, index) => {
                   return (
                     <li key={index} className="text-xl text-accent">
